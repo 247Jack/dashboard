658c261da4d3d11ae4a572a6f08d599ee8933869
Daniel Zurita <zurita.daniel@hotmail.com>
2018-08-06 17:51:31 -0500

    fixed env API

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
   okta_clientId: "0oaeu3zgonjGZw0qE0h7",
-  api_domain: "https://15kcv4z18f.execute-api.us-east-1.amazonaws.com/stg",
+  api_domain: "https://15kcv4z18f.execute-api.us-east-1.amazonaws.com/prod",
   broker_host: "ws://broker.247jack.com:15674/ws",
   broker_user: "user",
-  broker_pass: "7ojs2IaGvXM6",
+  broker_pass: "Nrl2OGopvVjB",
   self_host: "http://dashboard.247jack.com"
 };
