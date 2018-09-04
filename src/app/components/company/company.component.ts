57ac53758a7258490223b6d7c3429938e904a04e
Ale Rodriguez <alerodriguez162@gmail.com>
2018-09-04 00:01:32 -0500

    Add select in company component

--- src/app/components/company/company.component.ts
+++ src/app/components/company/company.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-company',
-  templateUrl: './company.component.html',
-  styleUrls: ['./company.component.css']
+  selector: "app-company",
+  templateUrl: "./company.component.html",
+  styleUrls: ["./company.component.css"]
 })
 export class CompanyComponent implements OnInit {
 
-  constructor() { }
+  itemList = [];
+  selectedItems = [];
+  settings = {};
 
+  constructor() {}
   ngOnInit() {
+    this.itemList = [
+      { id: 1, itemName: "Property 1"},
+      { id: 2, itemName: "Property 2" },
+      { id: 3, itemName: "Property 3" },
+    ];
+    this.settings = { singleSelection: true, text: "Select Property" };
   }
-
+  onItemSelect(item: any) {
+    console.log(item);
+  }
 }
