830145d728c94ed92e50b6fc00a3f27828074146
alerodriguez162 <42100837+alerodriguez162@users.noreply.github.com>
2018-10-16 18:22:05 -0500

    Change text in the work order form buttons

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from "@angular/core";
-import {StatsService } from "../../services/stats.service"
-import { Ng4LoadingSpinnerService } from 'ng4-loading-spinner';
+import { StatsService } from "../../services/stats.service";
+import { Ng4LoadingSpinnerService } from "ng4-loading-spinner";
 
 @Component({
   selector: "app-home",
@@ -8,9 +8,8 @@ import { Ng4LoadingSpinnerService } from 'ng4-loading-spinner';
   styleUrls: ["./home.component.css"]
 })
 export class HomeComponent implements OnInit {
-
-  public stats;
-  public currentPropertyManager;
+  public stats = Array;
+  public currentPropertyManager = Array;
   status: boolean = false;
   constructor(
     private spinnerService: Ng4LoadingSpinnerService,
@@ -24,19 +23,19 @@ export class HomeComponent implements OnInit {
       );
       if (this.currentPropertyManager) {
         clearInterval(waitForPMData);
-        this.loadStats()
-        console.log(this.currentPropertyManager)
+        this.loadStats();
       }
     }, 100);
   }
-
-  public loadStats(){
+  0;
+  public loadStats() {
     this.spinnerService.show();
-    this.stat.getStats(this.currentPropertyManager["_id"], 30)
-    .subscribe(data => {
-      this.stats = data
-      this.spinnerService.hide();
-      this.status = true
-    });
+    this.stat
+      .getStats(this.currentPropertyManager["_id"], 30)
+      .subscribe(data => {
+        this.stats = data;
+        this.spinnerService.hide();
+        this.status = true;
+      });
   }
 }
