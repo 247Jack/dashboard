48af415e5722043081af540074f58d69a4052ba3
Mario Ricardo Dominguez Quiroz <darkmarthur@gmail.com>
2018-10-24 20:12:17 -0500

    navBar optimization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,15 +3,26 @@ import { ActivatedRoute, Router } from "@angular/router";
 
 @Component({
   selector: "app-root",
-  templateUrl: "./app.component.html",
+  template: `
+  <div *ngIf="!isLogin; else login" class="app" id="app">
+    <app-navbar></app-navbar>
+    <router-outlet ></router-outlet>
+  </div>
+  <ng-template #login>
+    <router-outlet ></router-outlet>
+  </ng-template>  
+  `,
   styleUrls: ["./app.component.css"]
 })
+
 export class AppComponent {
   title: String = "Jack";
   path: String;
+  isLogin: boolean;
   constructor(private router: Router) {
     router.events.subscribe(val => {
       this.path = window.location.pathname;
+      this.isLogin = this.path.includes('login');
     });
   }
 }
