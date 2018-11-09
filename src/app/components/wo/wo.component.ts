44f366d02530dde97320bb67968ec7da5a638316
Daniel Zurita <zurita.daniel@hotmail.com>
2018-11-08 19:44:55 -0600

    completed edit WO function

--- src/app/components/wo/wo.component.ts
+++ src/app/components/wo/wo.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { Ng4LoadingSpinnerService } from 'ng4-loading-spinner';
 import { TicketsService } from "../../services/tickets.service";
 import { IssuesService } from "../../services/issues.service";
+import { ContactsService } from "../../services/contacts.service";
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ModalComponent } from 'dsg-ng2-bs4-modal';
 
@@ -21,20 +22,31 @@ export class WoComponent implements OnInit {
   public currentTicket;
   public limitRows = 8;
   public recivedData;
-  public vendorlist: any;
   public issueToken: any;
   public issue: any;
-  public selectedRow = [];
+  public selectedRow: any = [];
+  public selectedVendor: any;
   public selected = [];
   public editForm : FormGroup;
   public defaultStatus: string = 'Edit Status';
   public defaultVendor: string = 'Edit Vendor';
   public modalTitle = "";
   public modalBody = "";
+  public bitlyURL = "";
+  public newCost = 0;
+
+  public vendorList = [];
+  public vendorSelectedItems = [];
+  public vendorSettings = {};
+  public statusList = [];
+  public statusSelectedItems = [];
+  public statusSettings = {};
+
   constructor(
     private spinnerService: Ng4LoadingSpinnerService,
     private ticket: TicketsService,
-    private issues: IssuesService
+    private issues: IssuesService,
+    private contacts: ContactsService
   ) {
     this.editForm = new FormGroup({
       status: new FormControl(null),
@@ -44,6 +56,44 @@ export class WoComponent implements OnInit {
     });
     this.editForm.controls['status'].setValue(this.defaultStatus, {onlySelf: true})
     this.editForm.controls['vendor'].setValue(this.defaultVendor, {onlySelf: true})
+    this.vendorSettings = {
+      singleSelection: true,
+      text: "Edit vendor",
+      enableSearchFilter: true,
+      badgeShowLimit: 1
+    }
+    this.statusSettings = {
+      singleSelection: true,
+      text: "Edit status",
+      enableSearchFilter: false,
+      badgeShowLimit: 1
+    }
+    this.statusList = [
+      {
+        id: 0,
+        itemName: "available"
+      },
+      {
+        id: 1,
+        itemName: "checked"
+      },
+      {
+        id: 2,
+        itemName: "evaluating"
+      },
+      {
+        id: 3,
+        itemName: "approved"
+      },
+      {
+        id: 4,
+        itemName: "denied"
+      },
+      {
+        id: 5,
+        itemName: "finished"
+      }
+    ]
 }
 
   ngOnInit() {
@@ -58,11 +108,25 @@ export class WoComponent implements OnInit {
         this.ticket.update.subscribe(updating => {
           this.loadTasks();
         });
+        this.contacts
+          .getContacts(this.currentPropertyManager["_id"], this.currentCompany, "vendors")
+          .subscribe(listVendors => {
+            var vendorsList = [];
+            for (var v in listVendors["vendorsResult"])
+              vendorsList.push({
+                id: listVendors["vendorsResult"][v]._id,
+                itemName: `${
+                  listVendors["vendorsResult"][v]["vendorData"]["name"]
+                  } (${
+                  listVendors["vendorsResult"][v]["vendorData"]["jobType"]
+                  })`
+              });
+            this.vendorList = vendorsList;
+          });
       }
     }, 100);
     this.recivedData = this.issues.senddata.subscribe(data =>{
-      this.vendorlist = data
-      console.log(this.vendorlist)
+      this.vendorList = data
     })
   }
 
@@ -102,22 +166,68 @@ export class WoComponent implements OnInit {
   }
   onSelect({ selected }) {
     this.selectedRow = selected[0]
-    document.getElementById("editRow").click();
+    console.log(this.selectedRow)
+    if(this.selectedRow.assignedVendors.length > 0){
+      this.selectedVendor = this.selectedRow.assignedVendors[0]._id
+      this.vendorSelectedItems = []
+      this.vendorSelectedItems.push({
+        id: this.selectedRow.assignedVendors[0]._id,
+        itemName: this.selectedRow.assignedVendors[0].vendorData.name
+      })
+      this.ticket.getBitlyLink(this.selectedRow._id,this.selectedVendor).subscribe(resultBitly => {
+        this.bitlyURL = resultBitly.url;
+        document.getElementById("editRow").click();
+      })
+    }
+    else{
+      document.getElementById("editRow").click();
+    }
+    this.newCost = this.selectedRow.issueData.repairCost || 0
+    this.statusSelectedItems = []
+    for(var i in this.statusList){
+      if(this.statusList[i].itemName === this.selectedRow.status){
+        this.statusSelectedItems.push(this.statusList[i])
+        break
+      }
+    }
     if(selected[0]._id){
       this.editForm.controls['id'].setValue(selected[0]._id, {onlySelf: true})
     }
+    
   }
+
+  onVendorSelect(event){
+    this.ticket.getBitlyLink(this.selectedRow._id, event.id).subscribe(resultBitly => {
+      this.bitlyURL = resultBitly.url
+      //document.getElementById("editRow").click();
+    })
+  }
+
   cancelEdit(){
     this.selected = []
   }
   saveEdit(){
-    console.log(this.editForm.value)
-    this.modalShowMessage("");
+    var updateData = {
+      vendor_id: (this.vendorSelectedItems.length)?this.vendorSelectedItems[0].id:"",
+      status: (this.statusSelectedItems.length)?this.statusSelectedItems[0].itemName:"",
+      repair_cost: this.newCost
+    }
+    this.ticket.updateSimpleRequest(this.selectedRow._id, this.currentCompany, updateData).subscribe(resultUpdate => {
+      this.selected = []
+      this.modalShowMessage("saveTask");
+      this.loadTasks();
+    })
+    
   }
+
 
 
   public modalShowMessage(messageType) {
     switch (messageType) {
+      case "saveTask":
+        this.modalTitle = "Request updated!";
+        this.modalBody = "Your select request has been updated. However, you need to notify the vendors about the change, if needed.";
+      break;
       default: {
         this.modalTitle = "Oops!";
         this.modalBody = "Something went wrong on our end. Nothing terrible; however, you will need to enter the request information.";
