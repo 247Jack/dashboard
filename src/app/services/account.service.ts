6e6cfbbbed090313137377a5e45c312318590f2a
Daniel Zurita <zurita.daniel@hotmail.com>
2018-12-06 02:52:52 -0600

    Added in-app login

--- src/app/services/account.service.ts
+++ src/app/services/account.service.ts
@@ -2,14 +2,25 @@ import { Injectable } from '@angular/core';
 import { Http, Headers, RequestOptions } from "@angular/http";
 import { Subject } from "rxjs/Subject";
 import { Observable } from "rxjs/Observable";
+import * as OktaSignIn from '@okta/okta-signin-widget';
 import { environment } from "../../environments/environment";
 
 @Injectable()
 export class AccountService {
 
   public accountInfo : any;
+  private widget;
 
-  constructor(private http: Http) { }
+  constructor(private http: Http) {
+    this.widget = new OktaSignIn({
+      baseUrl: 'https://dev-825764.oktapreview.com',
+      clientId: environment.okta_clientId,
+      redirectUri: `${environment.self_host}/implicit/callback`,
+      authParams: {
+        issuer: 'default'
+      }
+    });
+   }
 
   public getAccountData(email) {
     return this.http
@@ -23,6 +34,10 @@ export class AccountService {
       .catch(error => {
         return error;
       });
+  }
+
+  public getWidget() {
+    return this.widget;
   }
 
   public getCurrentAccountData()
