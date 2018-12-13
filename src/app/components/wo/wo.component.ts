a201507fbcde26210f1d98a38a43f2be31107762
Daniel Zurita <zurita.daniel@hotmail.com>
2018-12-12 21:36:25 -0600

    Removed vendors from newVendorList

--- src/app/components/wo/wo.component.ts
+++ src/app/components/wo/wo.component.ts
@@ -202,9 +202,9 @@ export class WoComponent implements OnInit {
       this.ticket.getEditTask(this.currentPropertyManager['_id'], this.currentCompany, this.selectedRow._id).subscribe(data => {
         this.editDataRequest = data
         this.vendorList = this.staticVendorList;
-        let ids = this.editDataRequest.providersDispatched.map(v => (v.id).toString())
-        let newVendorList = this.vendorList.filter(v => !(ids.includes((v.id).toString())))
-        this.vendorList = newVendorList
+        //let ids = this.editDataRequest.providersDispatched.map(v => (v.id).toString())
+        //let newVendorList = this.vendorList.filter(v => !(ids.includes((v.id).toString())))
+        //this.vendorList = newVendorList
         this.ticket.getBitlyLink(this.selectedRow._id, this.selectedVendor).subscribe(resultBitly => {
           this.bitlyURL = resultBitly.url;
         })
